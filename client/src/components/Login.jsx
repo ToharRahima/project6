55f369e1ade9f7b80be3ce6a99ec2756a5cc8f20
rotemn2006 <rotemneugeboren@hilma.tech>
2024-11-20 13:24:44 +0200

    login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Login() {
-  return <div>L</div>;
+  return (
+    <>
+      <h1>login:</h1>
+      <form action="http://localhost:3000/login" method="POST">
+        <label for="username">username</label>
+        <input type="text" name="username" />
+        <label for="password">password</label>
+        <input type="text" name="password" />
+        <input type="submit" value="submit" />
+      </form>
+      <Link to="/signup">signup</Link>
+    </>
+  );
 }
