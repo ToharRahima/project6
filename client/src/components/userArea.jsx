9b389f2e32825a69d38dc687deaf37374179bab2
rotemn2006 <rotemneugeboren@hilma.tech>
2024-11-20 19:50:56 +0200

    add file

--- client/src/components/userArea.jsx
+++ client/src/components/userArea.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import File from "./File";
 export default function UserArea(props) {
   const [folderContent, setFolderContent] = useState([]);
+  const [newFile, setNewFile] = useState("");
+
   // TODO: deal with cases when there is no currentUser
   let username = JSON.parse(localStorage.getItem("currentUser"));
 
@@ -26,11 +28,34 @@ export default function UserArea(props) {
     };
     showFolder();
   }, []);
+
+  const addFile = async () => {
+    if (newFile) {
+      console.log("newFile: ", newFile);
+      await fetch(`http://localhost:3000/users/${username}`, {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({ filename: newFile }),
+      }).then((res) => {
+        console.log("res: ", res);
+        if (!res.ok) {
+          throw new Error("somthing went wrong");
+        } else {
+          setFolderContent((prev) => [...prev, newFile]);
+        }
+      });
+    }
+  };
+
   {
     if (username) {
       return (
         <>
-          <div>FOLDERS:</div>
+          <div id="addFile">
+            <h2>add new file</h2>
+            <input onChange={(e) => setNewFile(e.target.value)} />
+            <button onClick={addFile}>+</button>
+          </div>
           {folderContent.map((item) => (
             <File key={item} name={item} />
           ))}
